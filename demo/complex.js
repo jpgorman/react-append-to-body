f06542ff34d5a9ee1381de10cac8e262e240ff8d
JP Gorman <j.p.g.gorman@mgail.com>
2018-01-13 16:14:20 +0000

    Fix dependencies in demo

--- demo/complex.js
+++ demo/complex.js
@@ -1,103 +1,114 @@
 /* eslint-disable react/prop-types */
-import React from "react"
-import ReactDOM from "react-dom"
-import {Modal} from "./modal"
-import {append, filter, map, contains} from "../src/rambda"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Modal } from "./modal";
+import { append, filter, map, contains } from "rambda";
 
-function addIndex(functor){
+function addIndex(functor) {
   return function(fn, ...rest) {
-    let cnt = 0
-    const newFn = (args) => fn.apply(null, [args, cnt++])
-    return functor.apply(null, [newFn, ...rest])
-  }
+    let cnt = 0;
+    const newFn = args => fn.apply(null, [args, cnt++]);
+    return functor.apply(null, [newFn, ...rest]);
+  };
 }
 
-import {
-  componentWillAppendToBody
-} from "../src/"
+import { componentWillAppendToBody } from "../src/";
 
-const PortalA = componentWillAppendToBody(Modal)
-const PortalB = componentWillAppendToBody(Modal)
-const PortalC = componentWillAppendToBody(Modal)
+const PortalA = componentWillAppendToBody(Modal);
+const PortalB = componentWillAppendToBody(Modal);
+const PortalC = componentWillAppendToBody(Modal);
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       value: 0,
-      modals: [],
-    }
-    this.handler = this.handler.bind(this)
-    this.addModal = this.addModal.bind(this)
-    this.removeModal = this.removeModal.bind(this)
+      modals: []
+    };
+    this.handler = this.handler.bind(this);
+    this.addModal = this.addModal.bind(this);
+    this.removeModal = this.removeModal.bind(this);
   }
 
   handler(event) {
     this.setState({
-      value: event.target.value,
-    })
+      value: event.target.value
+    });
   }
 
   addModal(name) {
-    const modals = append(name, this.state.modals)
+    const modals = append(name, this.state.modals);
     this.setState({
-      modals,
-    })
+      modals
+    });
   }
 
   removeModal(names) {
-    const modals = filter((name) => !contains(name, names), this.state.modals)
+    const modals = filter(name => !contains(name, names), this.state.modals);
     this.setState({
-      modals,
-    })
+      modals
+    });
   }
 
   render() {
-
     const modalMap = {
       modalA: (
         <PortalA>
           <div>My First App</div>
-          <div id="inner-modal"></div>
+          <div id="inner-modal" />
           <div className="controls">
-            <button onClick={this.removeModal.bind(null, ["modalA", "modalC"])}>Close App</button>
-            <button key="btn3" onClick={this.addModal.bind(null, "modalC")}>Open App C in App A</button>
+            <button onClick={this.removeModal.bind(null, ["modalA", "modalC"])}>
+              Close App
+            </button>
+            <button key="btn3" onClick={this.addModal.bind(null, "modalC")}>
+              Open App C in App A
+            </button>
           </div>
         </PortalA>
       ),
       modalB: (
         <PortalB subtreeContainer={"#other-element-container"}>
           <div>My Second App</div>
-          <div><input onChange={this.handler} value={this.state.value} /></div>
-          <div><button onClick={this.removeModal.bind(null, ["modalB"])}>Close App</button></div>
+          <div>
+            <input onChange={this.handler} value={this.state.value} />
+          </div>
+          <div>
+            <button onClick={this.removeModal.bind(null, ["modalB"])}>
+              Close App
+            </button>
+          </div>
         </PortalB>
       ),
       modalC: (
         <PortalC subtreeContainer={"#inner-modal"}>
           <div>My Third App</div>
-          <button onClick={this.removeModal.bind(null, ["modalC"])}>Close App</button>
+          <button onClick={this.removeModal.bind(null, ["modalC"])}>
+            Close App
+          </button>
         </PortalC>
-      ),
-    }
+      )
+    };
 
     const buttons = [
-      <button key="btn1" onClick={this.addModal.bind(null, "modalA")}>Open App A</button>,
-      <button key="btn2" onClick={this.addModal.bind(null, "modalB")}>Open App B</button>,
-    ]
+      <button key="btn1" onClick={this.addModal.bind(null, "modalA")}>
+        Open App A
+      </button>,
+      <button key="btn2" onClick={this.addModal.bind(null, "modalB")}>
+        Open App B
+      </button>
+    ];
 
     return (
       <div>
         <div>This is my main app</div>
         <div>{buttons}</div>
-        {addIndex(map)((name, index) => <span key={index}>{modalMap[name]}</span>, this.state.modals)}
+        {addIndex(map)(
+          (name, index) => <span key={index}>{modalMap[name]}</span>,
+          this.state.modals
+        )}
       </div>
-    )
+    );
   }
 }
 
-
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("app")
-)
+ReactDOM.render(React.createElement(App), document.getElementById("app"));
