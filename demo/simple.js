a77a30cc0fa9d9f795d3e3b2689d6838d161f77d
JP Gorman <j.p.g.gorman@mgail.com>
2018-02-01 13:43:06 +0000

    Update test scripts to teardown react versions

--- demo/simple.js
+++ demo/simple.js
@@ -1,24 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Modal } from "./modal";
 
 import { componentWillAppendToBody } from "../src/";
 
-// const AppendedModal = componentWillAppendToBody(Modal)
-function AfterNav({ children }) {
-  return <div>{children}</div>;
-}
-
-const AppendedAfterNav = componentWillAppendToBody(AfterNav);
-// AppendedAfterNav.contextTypes = {
-//   router: PropTypes.any.isRequired
-// };
-
-const Dashboard = () => <div>Dashboard</div>;
-const Chores = () => <div>Chores</div>;
+const AppendedModal = componentWillAppendToBody(Modal);
 
 class App extends React.Component {
   constructor(props) {
@@ -28,33 +15,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Router>
-          <div>
-            <AppendedAfterNav>
-              <ul className="nav navbar-nav">
-                <li>
-                  <Link to="/chores">Chores</Link>
-                </li>
-                <li>
-                  <Link to="/dashboard">Dashboard</Link>
-                </li>
-              </ul>
-            </AppendedAfterNav>
-            <Switch>
-              <Route
-                path="/dashboard"
-                render={routeProps => <Dashboard {...routeProps} />}
-              />
-              <Route
-                path="/chores"
-                render={routeProps => <Chores {...routeProps} />}
-              />
-            </Switch>
-          </div>
-        </Router>
+        I'm the app
+        <AppendedModal>I'm rendered outside of the app </AppendedModal>
       </div>
     );
   }
 }
-
 ReactDOM.render(React.createElement(App), document.getElementById("app"));
